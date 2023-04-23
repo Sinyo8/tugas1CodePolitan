7d0c8058b0cc787135cf480c6364f5e6c73d4a2f
sinyo8 <dickyrizkynovebianto@gmail.com>
2023-04-23 19:01:27 +0700

    update all files

---
+++ asset/js/app.js
@@ -0,0 +1,37 @@
+$(".img").attr("data-bs-toggle", "modal");
+$(".img").attr("data-bs-target", "#staticBackdrop");
+function getImgUrl() {
+  $.ajax({
+    url: "https://pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com/api/server",
+    method: "POST",
+    headers: {
+      contentType: "application/json",
+      "X-RapidAPI-Key": "5c143f64c8mshf4fd357a2ea221cp123facjsn78b677113d6e",
+      "X-RapidAPI-Host":
+        "pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com",
+    },
+    data: {
+      id: "https://pin.it/5TZnIsk",
+    },
+    success: function (response, a) {
+      const imageUrl = response.data.image_medium_url;
+      console.log(imageUrl);
+      console.log(a);
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  });
+}
+
+// async function pushToArray(listUrl, getUrl) {}
+getImgUrl();
+// let count = 1;
+// const allImg = $(".img");
+// $("#leftBtn").on("click", function (e) {
+//   function counter() {
+
+//   }
+//   counter();
+//   console.log(count);
+// });
